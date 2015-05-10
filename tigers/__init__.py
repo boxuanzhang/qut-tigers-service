8d1d78b3a818d8dbbfc712f80d37d779059965f2
boxuanzhang <boxuan.zhang@student.qut.edu.au>
2015-05-10 10:35:49 +0800

    Register user resource

--- tigers/__init__.py
+++ tigers/__init__.py
@@ -1,6 +1,6 @@
 
 __all__ = [
-    'app'
+    'app', 'permission_manager'
 ]
 
 #
@@ -29,6 +29,9 @@ connect(
     port=app.config['MONGODB_PORT'],
 )
 
+# Permission Manager
+from .decorators.permission import PermissionsManager
+permission_manager = PermissionsManager()
 
 #
 # Build RESTful service
@@ -38,15 +41,20 @@ from flask.ext.restful import Api
 api = Api(app)
 
 # Auth
-from tigers.resources.auth import Auth
+from .resources.auth import Auth
 api.add_resource(Auth, '/auth')
 
 # Status
-from tigers.resources.status import Status, StatusList
+from .resources.status import Status, StatusList
 api.add_resource(StatusList, '/status')
 api.add_resource(Status, '/status/<status_id>')
 
 # Photo
-from tigers.resources.photo import Photo, PhotoToken
+from .resources.photo import Photo, PhotoToken
 api.add_resource(Photo, '/photo/<photo_id>')
 api.add_resource(PhotoToken, '/photo_token')
+
+# User
+from .resources.user import User, UserList
+api.add_resource(UserList, '/user')
+api.add_resource(User, '/user/<user_id>')
